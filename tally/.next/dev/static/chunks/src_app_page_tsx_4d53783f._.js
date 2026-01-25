fc2c9e29e1dd244b893e6f0d22c94bbebe23393d
Parvinjeet Kaur <parvinjeet.kaur@gmail.com>
2026-01-25 11:11:40 +0800

    Remove build artifacts from repo

--- tally/.next/dev/static/chunks/src_app_page_tsx_4d53783f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_9f36b662._.js",
-  "static/chunks/src_5afa318b._.js"
-],
-    source: "dynamic"
-});
